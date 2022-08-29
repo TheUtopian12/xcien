8549c28da02325f672e61bb248f0961d041946aa
Edwin Castruita <edwin.castruita@wispi.mx>
2022-08-29 10:28:41 -0500

    Pagina principal

--- components/main/Hero.jsx
+++ components/main/Hero.jsx
@@ -47,8 +47,8 @@ const Hero = () => {
             </h1>
           </div>
           <div className="col-end-4 text-white mt-[2rem] lg:ml-[-5rem] w-10/12 text-center">
-            <button class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-lg font-medium text-gray-900 rounded-full group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 w-52">
-              <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-full group-hover:bg-opacity-0 w-52">
+            <button className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-lg font-medium text-gray-900 rounded-full group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 w-52">
+              <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-full group-hover:bg-opacity-0 w-52">
                 Ver Servicios
               </span>
             </button>
