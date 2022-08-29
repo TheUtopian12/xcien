0ded21216b5191aae590e453caf23f3c424b72d3
Edwin Castruita <edwin.castruita@wispi.mx>
2022-08-29 13:28:56 -0500

    Animacion a tarjetas expandidas

--- components/ventajas/Ventajas.jsx
+++ components/ventajas/Ventajas.jsx
@@ -3,32 +3,33 @@ import Expanding from "./Expanding";
 const images = [
   {
     id: 1,
-    url: "",
+    url: "/img/Back01.png",
     title: "Integramos servicios",
     description:
       "Todos tus servicios de telecomunicaciones en un mismo lugar. Juntos podemos desarrollar e integrar las mejores soluciones que necesita tu empresa.",
+    active: true,
   },
   {
     id: 2,
-    url: "",
+    url: "/img/Back02.png",
     title: "Servicios administrados",
     description:
       "Simplifica la administración de tus servicios de telecomunicaciones a través de la migración a la nube, la transformación digital y las redes mejoradas. ¡Despreocúpate! Nosotros nos encargamos de tus servicios con personal especializado.",
-  },
+      active: false,},
   {
     id: 3,
-    url: "",
+    url: "/img/Back03.png",
     title: "Escalabilidad",
     description:
       "Si tu empresa está en constante crecimiento e innovación, somos tu mejor aliado. Nos encargamos de crecer a tu ritmo para que tus servicios de telecomunicaciones se ajusten a los requerimientos de tu empresa. ",
-  },
+  active: false},
   {
     id: 4,
-    url: "",
+    url: "/img/Back04.png",
     title: "SLA / Nivel de Servicio",
     description:
       "Todos nuestros procesos y tecnología están diseñados para ofrecerte los más altos niveles de servicio y brindarte una disponibilidad de hasta 99.95% anual en tu servicio de internet",
-  }
+  active: false},
 ];
 const Ventajas = () => {
   return (
@@ -40,9 +41,7 @@ const Ventajas = () => {
         backgroundSize: "cover",
       }}
     >
-
-      <Expanding/>
-
+      <Expanding data={images} />
     </div>
   );
 };
