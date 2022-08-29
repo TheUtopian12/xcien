7e213e3c77693842b218c7965dbca1868a19d144
Edwin Castruita <edwin.castruita@wispi.mx>
2022-08-29 13:14:12 -0500

    Animacion a tarjetas expandidas

--- components/ventajas/Expanding.jsx
+++ components/ventajas/Expanding.jsx
@@ -1,22 +1,22 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import Card from "./Card";
 
 const Expanding = (props) => {
-  const [data, setData] =useState(props.data)
-  const onCardClick =(id)=>{
-    const newState =[...data]
-    newState.map((item) => item.id === id ? item.active = true : item.active =false )
-    setData(newState)
-  }
-  if (!data && data.length <= 0) {
-    return "No data";
-  }
+  const [data, setData] = useState(props.data);
+  const onCardClick = (id) => {
+    const newState = [...data];
+    newState.map((item) =>
+      item.id === id ? (item.active = true) : (item.active = false)
+    );
+    setData(newState);
+  };
+ 
 
   return (
     <section className="cards">
       {data.map((card) => (
-        <Card key={card.id} data={card} onCardClick={onCardClick}/>
-    ))}
+        <Card key={card.id} data={card} onCardClick={onCardClick} />
+      ))}
     </section>
   );
 };
