9bdd79c13f59c4d32c9030939bfdb09e20d88ba4
Edwin Castruita <edwin.castruita@wispi.mx>
2022-08-31 18:51:53 -0500

    Se agrego responsivo ventajas

--- components/ventajas/CardVertical.jsx
+++ components/ventajas/CardVertical.jsx
@@ -4,14 +4,15 @@ const CardVertical = (props) => {
   const { id, url, active, title, description } = props.data;
 
   return (
-    <div
+    <div onTouchStartCapture={() => props.onCardClick(id)}> <div
       className={` cardV ${active && "active"}`}
       style={{ backgroundImage: `url(${url})` }}
-      onMouseEnter={() => props.onCardClick(id)}
+      
     >
       <h3>{title}</h3>
       <p>{description}</p>
-    </div>
+    </div></div>
+   
   );
 };
 
