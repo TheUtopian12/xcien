b9a2ad597fc22ba8c5dd0a2e076e86678c6cb9eb
Edwin Castruita <edwincastruita@MacBook-Air-de-Jesus-Canamar.local>
2022-09-05 14:35:31 -0500

    Se corrige responsivo en LG

--- components/main/Example.jsx
+++ components/main/Example.jsx
@@ -29,7 +29,7 @@ const Example = () => {
                     md:w-[1200px]
                     md:translate-y-[800px]
                     lg:w-[1370px]
-                    lg:translate-y-[820px]
+                    lg:translate-y-[720px]
                     2xl:w-[1800px]
                     2xl:h-[600px]
                     2xl:translate-y-[710px]
@@ -121,7 +121,7 @@ const Example = () => {
                       </button>
                     </div>
 
-                    <div className="mt-[110px] md:mt-[280px] xl:translate-y-[90px] 2xl:-translate-y-[130px] 2xl:w-[1200px] 2xl:-translate-x-[770px] ">
+                    <div className="mt-[110px] md:mt-[100px]   xl:translate-y-[90px] 2xl:-translate-y-[130px] 2xl:w-[1200px] 2xl:-translate-x-[770px] ">
                       <h1 className="font-extrabold text-transparent text-2xl 2xl:text-3xl  bg-clip-text bg-gradient-to-r from-cyan-400 via-green-500 to-green-400 text-center ">
                         Somos el operador inalámbrico empresarial más grande de
                         México
