76a24e980aad66399291fc12b8e72ef4180ea4fd
Edwin Castruita <edwin.castruita@wispi.mx>
2022-09-09 17:45:05 -0500

    Se agrego spinner al cargar pagina

--- components/opiniones/Empresas2.jsx
+++ components/opiniones/Empresas2.jsx
@@ -18,29 +18,9 @@ export default class MultipleItems extends Component {
     };
     return (
 
-      <><div className="2xl:w-[1800px] sm:w-[1280px] w-[360px] pr-10 pl-10 sm:pr-36 sm:pl-36 text-center ">
-        <Slider {...settings2} className="text-center">
-          <div className="ml-[25%]">
-            <img src="/img/Empresas 2/Aiisa.png" className="w-40" />
-          </div>
-          <div className="ml-[25%]">
-            <img src="/img/Empresas 2/Assetel.png" className="w-40" />
-          </div>
-          <div className="ml-[25%]">
-            <img src="/img/Empresas 2/Coeficiente.png" className="w-40" />
-          </div>
-          <div className="ml-[25%]">
-            <img src="/img/Empresas 2/cogent.png" className="w-40" />
-          </div>
-          <div className="ml-[25%]">
-            <img src="/img/Empresas 2/Evengroup.png" className="w-40" />
-          </div>
-          <div className="ml-[25%]">
-            <img src="/img/Empresas 2/Sparkle.png" className="w-40" />
-          </div>
-        </Slider>
-      </div>
-      <div className="2xl:w-[1800px] sm:w-[1280px] w-[360px] pr-10 pl-10 sm:pr-36 sm:pl-36 text-center hidden">
+      <>
+      
+      <div className="hidden sm:flex"><div className="2xl:w-[1800px] sm:w-[1280px] w-[360px] pr-10 pl-10 sm:pr-36 sm:pl-36 text-center ">
         <Slider {...settings} className="text-center">
           <div className="ml-[25%]">
             <img src="/img/Empresas 2/Aiisa.png" className="w-40" />
@@ -61,7 +41,30 @@ export default class MultipleItems extends Component {
             <img src="/img/Empresas 2/Sparkle.png" className="w-40" />
           </div>
         </Slider>
-      </div>
+      </div></div>
+      
+      <div className="flex sm:hidden"><div className="2xl:w-[1800px] sm:w-[1280px] w-[360px] pr-10 pl-10 sm:pr-36 sm:pl-36 text-center ">
+        <Slider {...settings2} className="text-center">
+          <div className="ml-[25%]">
+            <img src="/img/Empresas 2/Aiisa.png" className="w-40" />
+          </div>
+          <div className="ml-[25%]">
+            <img src="/img/Empresas 2/Assetel.png" className="w-40" />
+          </div>
+          <div className="ml-[25%]">
+            <img src="/img/Empresas 2/Coeficiente.png" className="w-40" />
+          </div>
+          <div className="ml-[25%]">
+            <img src="/img/Empresas 2/cogent.png" className="w-40" />
+          </div>
+          <div className="ml-[25%]">
+            <img src="/img/Empresas 2/Evengroup.png" className="w-40" />
+          </div>
+          <div className="ml-[25%]">
+            <img src="/img/Empresas 2/Sparkle.png" className="w-40" />
+          </div>
+        </Slider>
+      </div></div>
       
       </>
       
--- pages/_app.js
+++ pages/_app.js
@@ -9,7 +9,7 @@ function MyApp({ Component, pageProps }) {
   const [loading, setLoading] = React.useState(true)
   setTimeout(()=>{
     setLoading(false)
-  },5000)
+  },3000)
   return (
     <>
 
