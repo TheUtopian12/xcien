04e23a6701dce7c52291f72ed1e38934baf12212
Edwin Castruita <edwin.castruita@wispi.mx>
2022-09-21 09:50:10 -0500

    Se agrego gif de fondo

--- components/movil/Movil.jsx
+++ components/movil/Movil.jsx
@@ -12,70 +12,70 @@ const Movil = () => {
   return (
 
     <Parallax bgImage="/img/BG_S04.png" bgImageAlt="the cat" strength={500} bgImageStyle={{
-     
-      objectFit: 'cover', 
+
+      objectFit: 'cover',
     }}>
-    
-    <div className="w-full pt-36 xl:pt-0  2xl:pt-0 xl:h-[60vh] xl:pb-0  2xl:h-[50vh] h-full  md:-translate-y-[00px]">
-   
-    <h1 className="font-extrabold text-transparent text-2xl 2xl:text-3xl  bg-clip-text bg-gradient-to-b from-green-500 via-blue-500 to-green-500 text-center pt-10">
-              <span className="text-white">Descarga nuestra</span> xcienApp
-            </h1>
-      <div className="grid sm:grid-cols-3 ">
-        <div className="flex items-center justify-center pt-10">
-          <div className="w-[200px] sm:w-[400px] ">
-            <img src="/img/telefon.png" width={290} height={290} />
-          </div>
-        </div>
-        
-        <div className="sm:col-span-2 pt-10">
-          <div className="text-center">
-            
-            <h1 className="sm:text-4xl text-xl font-bold text-[#2adc2f] "></h1>
-          </div>
-          <div className="xl:flex sm:justify-between m-12 md:text-center text-white font-semibold grid grid-cols-1 lg:grid-cols-2 text-center">
-            <div>
-              <div className="w-[60px] mb-2 mt-5 ml-[40%]">
-                <Image src={view} width={100} height={80} />
-              </div>
-              <p className="pt-2 md:text-xs lg:text-base">
-                Visualiza el estado de tus servicios y sus consumos
-              </p>
-            </div>
 
-            <div>
-              {" "}
-              <div className="w-[60px] mb-2 mt-5 ml-[40%]">
-                <Image src={tech} width={100} height={90} />
-              </div>
-              <p className="md:text-xs lg:text-base">
-                Genera y da seguimiento a reportes de servicio
-              </p>
+      <div className="w-full pt-36 xl:pt-0  2xl:pt-0 xl:h-[60vh] xl:pb-0  2xl:h-[50vh] h-full  md:-translate-y-[00px]">
+
+        <h1 className="font-extrabold text-transparent text-2xl 2xl:text-3xl  bg-clip-text bg-gradient-to-b from-green-500 via-blue-500 to-green-500 text-center pt-10">
+          <span className="text-white">Descarga nuestra</span> <span className="text-[#2adc2f]">xcienApp</span> 
+        </h1>
+        <div className="grid sm:grid-cols-3 ">
+          <div className="flex items-center justify-center pt-10">
+            <div className="w-[200px] sm:w-[400px] ">
+              <img src="/img/telefon.png" width={290} height={290} />
             </div>
-            <div>
-              <div className="w-[60px] mb-2 mt-5 ml-[40%]">
+          </div>
+
+          <div className="sm:col-span-2 pt-10">
+            <div className="text-center">
+
+              <h1 className="sm:text-4xl text-xl font-bold text-[#2adc2f] "></h1>
+            </div>
+            <div className="xl:flex sm:justify-between m-12 md:text-center text-white font-semibold grid grid-cols-1 lg:grid-cols-2 text-center">
+              <div>
+                <div className="w-[60px] mb-2 mt-5 ml-[40%]">
+                  <Image src={view} width={100} height={80} />
+                </div>
+                <p className="pt-2 md:text-xs lg:text-base">
+                  Visualiza el estado de tus servicios y sus consumos
+                </p>
+              </div>
+
+              <div>
                 {" "}
-                <Image src={refresh} width={100} height={90} />
+                <div className="w-[60px] mb-2 mt-5 ml-[40%]">
+                  <Image src={tech} width={100} height={90} />
+                </div>
+                <p className="md:text-xs lg:text-base">
+                  Genera y da seguimiento a reportes de servicio
+                </p>
               </div>
-              <p className="md:text-xs lg:text-base">
-                Solicita una actualización de tu servicio
-              </p>
-            </div>
-            <div>
-              <div className="w-[60px] mb-2 mt-5 ml-[40%]">
-                <Image src={invoice} width={100} height={90} />
+              <div>
+                <div className="w-[60px] mb-2 mt-5 ml-[40%]">
+                  {" "}
+                  <Image src={refresh} width={100} height={90} />
+                </div>
+                <p className="md:text-xs lg:text-base">
+                  Solicita una actualización de tu servicio
+                </p>
               </div>
-              <p className="md:text-xs lg:text-base">Descarga tus facturas</p>
+              <div>
+                <div className="w-[60px] mb-2 mt-5 ml-[40%]">
+                  <Image src={invoice} width={100} height={90} />
+                </div>
+                <p className="md:text-xs lg:text-base">Descarga tus facturas</p>
+              </div>
             </div>
-          </div>
 
-          <div className="flex sm:justify-center sm:gap-40 justify-between ml-5 mr-5 sm:mr-0 sm:ml-0">
-            <Image src={Google} width={150} height={50} />
-            <Image src={AppStore} width={150} height={50} />
+            <div className="flex sm:justify-center sm:gap-40 justify-between ml-5 mr-5 sm:mr-0 sm:ml-0">
+              <Image src={Google} width={150} height={50} />
+              <Image src={AppStore} width={150} height={50} />
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </Parallax>
   );
 };
